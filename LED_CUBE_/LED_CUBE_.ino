c6da80260fcb05332b72d39e87d9ede22b829e1b
Vishnu <vkarpuram@gmail.com>
2018-12-09 19:36:09 -0500

    Added I2C support for additional GPIO pins

--- LED_CUBE_/LED_CUBE_.ino
+++ LED_CUBE_/LED_CUBE_.ino
@@ -45,14 +45,6 @@ void pcf8574_write(byte addr, byte value) {
 }
 
 
-
-int latches[] = {5, 6, 7, 8, 9, 10, 11, 12};
-int swiches[] = {3, 4, A0, A1, A2, A3, A4, A5}; 
-int pinSet[] = {0, 1, 2, 3, 4, 5, 6, 7};
-   
-
-
-
 void setup() 
 {
     Wire.begin();
